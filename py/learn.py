7dc270b38370c478dc31dc0484942a9085c53b20
Your Name <you@example.com>
2025-12-17 15:29:19 +0100

    learning

--- py/learn.py
+++ py/learn.py
@@ -13,12 +13,24 @@ an schrett vir schrett maan.
 import sys
 import random as r
 
+#also, ech muss just all indexes len(row) aus allen Rows driwwer huelen
+
+def genUniqueRow(*aboveRows):
+	row = []
+	nbrs = [i for i in range(10)]
+	while len(row) < 10:
+		aboveNbrs = []
+		nb = r.choice(nbrs)
+		row.append(nb)
+		nbrs.pop(nbrs.index(nb))
+	print("row:", row)
+
+def test():
+	genUniqueRow()
+
+test()
+
 def getLine(sGs, line):
-	#line 0 is the first line of sG0, the first line of sG1 and also for sG2
-	#line 1 is the second line
-	#line 2 is the third line
-	#line 3 is the first line of sG3, sG4 and sG5
-	#line 6 is the first line of sG6, sG7 and sG8
 	if line == 0: return sGs[0][0:3] + sGs[1][0:3] + sGs[2][0:3]
 	if line == 1: return sGs[0][3:6] + sGs[1][3:6] + sGs[2][3:6]
 	if line == 2: return sGs[0][6:9] + sGs[1][6:9] + sGs[2][6:9]
@@ -28,24 +40,25 @@ def getLine(sGs, line):
 	if line == 6: return sGs[6][0:3] + sGs[7][0:3] + sGs[8][0:3]
 	if line == 7: return sGs[6][3:6] + sGs[7][3:6] + sGs[8][3:6]
 	if line == 8: return sGs[6][6:9] + sGs[7][6:9] + sGs[8][6:9]
+
+def printLine(line, hide):
+	for i, c in enumerate(line):
+		if i % 3 == 0: print('|', end='')
+		if hide > r.randint(0, 9): print(' ', end='')
+		else: print(c, end='')
+	print('|\n', end='')
+
+def printSoduko(sGs, hide):
+	print("="*30)
+	for i in range(9): printLine(getLine(sGs, i), hide)
+
 def genField():
 	sGs = [[r.randint(0, 9) for i in range(9)] for i in range(9)]
-	for i, g in enumerate(sGs): print(g)	
-	print("="*30)
-	print(getLine(sGs, 0))
-	print(getLine(sGs, 1))
-	print(getLine(sGs, 2))
-	print(getLine(sGs, 3))
-	print(getLine(sGs, 4))
-	print(getLine(sGs, 5))
-	print(getLine(sGs, 6))
-	print(getLine(sGs, 7))
-	print(getLine(sGs, 8))
+	return sGs
 
 def	main():
 	av = sys.argv
 	ac = len(av)
-#	if ac != 2: return
-	genField()
-
-if __name__ == '__main__': main()
+	sGs = genField()
+	printSoduko(sGs, 0)
+#if __name__ == '__main__': main()
